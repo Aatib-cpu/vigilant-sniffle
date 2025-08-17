003d03548c8098439ce1a542bded2ccbb929574a
Aatib-cpu <aatib123@gmail.com>
2025-08-17 20:54:11 +0530

    yesterday commit

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./data.json";
-const date = moment().format();
+const date = moment().subtract(1,'d').format();
+//const date = moment().format();
 
 const data = {
     date: date,
 };
 
-jsonfile.writeFile(path, date);
-
-simpleGit().add([path]).commit(date,{'--date':date}).push();
+jsonfile.writeFile(path, date,()=>{simpleGit().add([path]).commit(date,{'--date':date}).push();});
